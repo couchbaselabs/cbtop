6662336d5e8b07e005f457f488730b4a294d7a4b
pavel-paulau <pavel.paulau@gmail.com>
2013-02-19 04:52:31 +0300

    migrate cbtop project from cbmonitor

---
+++ libcbtop/handler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+class Handler(object):
+    """
+    Base class for data handlers
+    """
+    def handle(self, source):
+        """
+        Consume and handle a data source
+        """
+        raise NotImplementedError(
+            "handle() has not been implemented")
